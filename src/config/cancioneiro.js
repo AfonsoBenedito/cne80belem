31f892e15776b6b90041dfa924ba235bdce70daf
AfonsoBenedito <afonso.benedito23@gmail.com>
2026-04-09 16:19:10 +0100

    feat(cancioneiro): add Estrela Polar with capo on 2nd fret

--- src/config/cancioneiro.js
+++ src/config/cancioneiro.js
@@ -451,4 +451,46 @@ Somos o Teu [F]povo peca[G]dor
 [F]Toma a nossa [G]vida de pe[C]cado e [Am]dor
 [F]Enche-nos do [G]Espírito de a[C]mor`,
   },
+  {
+    slug: 'estrela-polar',
+    title: 'Estrela Polar',
+    author: '',
+    youtubeId: '1y07suA6SBU',
+    key: 'Em',
+    capo: 2,
+    lyricsWithChords: `{Intro: Em C D Em Bm C D}
+
+[Em]Esta sede de te encontrar em [G]mim
+[Bm]De correr p'ra ti, de estar junto de [C]ti
+[Am]Guias pelos vales o decurso do meu [Em]rio
+[C]Única razão és [D]Tu. [Bm]Único sustento és [Em]Tu
+[C]A minha vida [D]existe porque existes [G]Tu.
+
+{R}[Em]Tudo gira à Tua [C]volta, em função de [D]Ti, [Em]
+[Bm]Não importa [C]quando, onde e o por[D]quê.
+
+[Em]Gira o firmamento sem nunca ter [G]paz
+[Bm]Mas existe um ponto a brilhar p'ra [C]mim
+[Am]A estrela polar que guia os meus [Em]passos.
+[C]A estrela polar és [D]Tu. [Bm]A estrela segura [Em]Tu
+[C]A minha vida [D]existe porque existes [G]Tu.
+
+{R}[Em]Tudo gira à Tua [C]volta, em função de [D]Ti, [Em]
+[Bm]Não importa [C]quando, onde e o por[D]quê.
+
+[Em]Brilha a Tua luz no centro do meu [G]ser,
+[Bm]Dás sentido à vida que em mim nas[C]ceu,
+[Am]Tudo o que farei será somente [Em]amor.
+[C]Única razão és [D]Tu. [Bm]A estrela polar [Em]Tu
+[C]A minha vida [D]existe porque existes [G]Tu.
+
+{R}[Em]Tudo gira à Tua [C]volta, em função de [D]Ti, [Em]
+[Bm]Não importa [C]quando, onde e o por[D]quê.
+
+[Em]Esta sede de te encontrar em [G]mim
+[Bm]De correr p'ra ti, de estar junto de [C]ti
+[Am]Guias pelos vales o decurso do meu [Em]rio
+[C]Única razão és [D]Tu. [Bm]Único sustento és [Em]Tu
+[C]A minha vida [D]existe porque existes [G]Tu.`,
+  },
 ];
