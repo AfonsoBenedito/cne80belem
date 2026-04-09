4c56a0f0d12da02e76c15ec4fbb6837cec0e4559
AfonsoBenedito <afonso.benedito23@gmail.com>
2026-04-09 16:17:53 +0100

    feat(cancioneiro): add Senhor, Tem Piedade de Nós

--- src/config/cancioneiro.js
+++ src/config/cancioneiro.js
@@ -427,4 +427,28 @@ Tem piedade de [G]nós [C] -----[G]
 Tem piedade de nós...
 Dá-nos a Tua paz...`,
   },
+  {
+    slug: 'senhor-tem-piedade-de-nos',
+    title: 'Senhor, Tem Piedade de Nós',
+    author: '',
+    youtubeId: 'yIlf9Kyn6cE',
+    key: 'C',
+    lyricsWithChords: `[C]Senhor, piedade de [F]nós [C]
+Somos o Teu [F]povo peca[G]dor
+
+[F]Toma a nossa [G]vida de pe[C]cado e [Am]dor
+[F]Enche-nos do [G]Espírito de a[C]mor
+
+[C]Cristo, piedade de [F]nós [C]
+Somos o Teu [F]povo peca[G]dor
+
+[F]Toma a nossa [G]vida de pe[C]cado e [Am]dor
+[F]Enche-nos do [G]Espírito de a[C]mor
+
+[C]Senhor, piedade de [F]nós [C]
+Somos o Teu [F]povo peca[G]dor
+
+[F]Toma a nossa [G]vida de pe[C]cado e [Am]dor
+[F]Enche-nos do [G]Espírito de a[C]mor`,
+  },
 ];
