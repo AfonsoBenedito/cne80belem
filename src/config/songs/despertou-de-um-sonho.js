910a9474c5a4001771d6b7ba14a62dade87a5f3d
AfonsoBenedito <afonso.benedito23@gmail.com>
2026-04-10 20:56:23 +0100

    feat: add 'Despertou de um Sonho' to cancioneiro

---
+++ src/config/songs/despertou-de-um-sonho.js
@@ -0,0 +1,25 @@
+export default {
+  slug: 'despertou-de-um-sonho',
+  title: 'Despertou de um Sonho',
+  author: '',
+  youtubeId: 'v6gFSwUWU18',
+  key: 'G',
+  lyricsWithChords: `[G]Despertou de um [C]sonho... um homem [G]viu... 
+E fez nas[C]cer em cada [G]olhar a [Bm]semente da[C] nova[Am] alegria[D].
+[G]Dando a mão a quem [C]precisa, [G]sempre certos da d[C]ivisa,
+[G]caminhando [Bm]monte [C]acima [Am]renovando a v[D]ida.
+
+{R}[G]Olhar o mundo como [D]ele o vê
+[G]queremos ser a imagem d[D]e B.P..
+[C]O topo está mais perto, dentro [D]de nós,
+[C]vamos levar ao mundo a [D]sua voz;
+[Bm]a sua voz, a razão de v[Em]iver, o seu [Bm]viver para constru[Em]ir,
+deixando o [Am]mundo sempre um po[D]uco melhor,
+sempre [Am]alerta para ser[D]vir.
+
+E, quando ele olhou em seu redor,
+não reparou que a noite, agora, não era tão noite.
+E, ao romper de um novo dia,
+vendo a gente que o seguia,
+fez-se raiz de uma árvore, renovando a vida.`,
+};
