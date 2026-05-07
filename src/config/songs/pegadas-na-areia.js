2b3aaadfb2488e139fe8e390a9231d2f08bdf134
AfonsoBenedito <afonso.benedito23@gmail.com>
2026-05-07 12:09:44 +0100

    feat(cancioneiro): add Pegadas na Areia

---
+++ src/config/songs/pegadas-na-areia.js
@@ -0,0 +1,34 @@
+export default {
+  slug: 'pegadas-na-areia',
+  title: 'Pegadas na Areia',
+  author: 'Maresia',
+  youtubeId: 'y4IPXUZvTSM',
+  key: 'E',
+  tags: ['oração'],
+  lyricsWithChords: `{Intro: E A (x2)}
+
+Uma n[E]oite tive um sonho
+Sonhei que andava a passear
+Eram d[A]ois pares de pegadas
+Mesmo ali à beirinha do m[E]ar [A]
+
+Vi a v[E]ida de cena em cena
+Mesmo ali ao pé de mim
+Voei c[A]omo uma gaivota
+Sem destino sem fim
+
+Enquanto s[E]obre as areias
+Apenas um pé a andar
+Levast[A]e-me tu ao colo
+Caminhando sobre o m[E]ar
+
+{R}Peg[E]adas na areia, um tr[A]ilho teu
+Talv[E]ez o que quis[A]este marc[B]ar.
+
+{Intro: E A (x2)}
+
+De peg[E]ada em pegada
+Deixei o meu trilho marcado
+Numa ar[A]eia, numa vida
+Sempre contigo ali, ao meu l[E]ado (Ao meu l[A]ado)`,
+};
